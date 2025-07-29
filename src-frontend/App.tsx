533208ece478c0e75274260533161d4f6d9b89a8
kenylson23 <kenylsonlourenco0@gmail.com>
2025-07-29 21:25:48 +0100

    fix: simplify App.tsx to debug Vercel blank page issue - remove complex dependencies temporarily

--- src-frontend/App.tsx
+++ src-frontend/App.tsx
@@ -1,36 +1,25 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ToastProvider } from "./components/ui/toast";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
-import Login from "./pages/Login";
-import Admin from "./pages/Admin";
-import Kitchen from "./pages/Kitchen";
-import OrderTracking from "./pages/OrderTracking";
-import NotFound from "./pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route path="/menu" component={Menu} />
-      <Route path="/login" component={Login} />
-      <Route path="/admin" component={Admin} />
-      <Route path="/cozinha" component={Kitchen} />
-      <Route path="/rastreamento" component={OrderTracking} />
-      <Route component={NotFound} />
+      <Route path="/menu" component={Home} />
+      <Route path="/login" component={Home} />
+      <Route path="/admin" component={Home} />
+      <Route path="/cozinha" component={Home} />
+      <Route path="/rastreamento" component={Home} />
+      <Route component={Home} />
     </Switch>
   );
 }
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <Router />
-      </ToastProvider>
-    </QueryClientProvider>
+    <div className="min-h-screen bg-gray-50">
+      <Router />
+    </div>
   );
 }
 
